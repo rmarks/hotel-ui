6a09c7be7bdd0ffb7acad4193160170e49ef4455
Raul <raul.marks@outlook.com>
2024-08-01 19:51:48 +0300

    Added visitor list and form pages

---
+++ Hotel.UI/Features/Visitors/VisitorListModel.cs
@@ -0,0 +1,3 @@
+﻿namespace Hotel.UI.Features.Visitors;
+
+public record VisitorListModel(int Id, string Name, string Email);
---
+++ Hotel.UI/Features/Visitors/VisitorModel.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Hotel.UI.Features.Visitors;
+
+public class VisitorModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string IdCode { get; set; } = "";
+    public string Email { get; set; } = "";
+}
+
+public class VisitorModelValidator : AbstractValidator<VisitorModel>
+{
+    public VisitorModelValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.IdCode).NotEmpty();
+        RuleFor(x => x.Email).NotEmpty();
+    }
+}
